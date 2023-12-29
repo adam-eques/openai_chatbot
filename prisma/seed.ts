26c4c56a9aa192bf176425915c2a883ec1d74422
acentior <draceques@gmail.com>
2023-12-29 21:16:55 +0000

    seed updated

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from '@prisma/client';
+import { loadOpenAIAssistant } from '../src/utils';
 import { createTable } from '../src/utils/airtables';
 const prisma = new PrismaClient()
 async function main() {
@@ -29,6 +30,40 @@ async function main() {
     }
   });
   console.log({ testClient })
+
+  if (typeof process.env.OPENAI_API_KEY !== "string") return
+  for (let index = 2; index <= 11; index++) {
+    const clientName = `client${index}`
+    const atbToken = "pat49tWyYSd5wejEM.8bf15fe554cce19b5a380718ae5d248c62140263fbf9fd8f9a3b0d3eacf0068d"
+    const atbBaseId = "appBTHW4e1DXYYFj9"
+    const atbTableNM = `leadInfo_${index}`
+    let atbTableId = "test"
+    try {
+      const res = await createTable(atbToken, atbBaseId, atbTableNM)
+      atbTableId = res.data.id
+      console.log(res.data)
+    } catch (error) {
+      console.log(error?.response?.data)
+      console.log(error?.response)
+      console.error("Failed to insert test data")
+      return
+    }
+
+    const assistant = await loadOpenAIAssistant(process.env.OPENAI_API_KEY, "")
+    const testClient = await prisma.client.upsert({
+      where: { id: index },
+      update: {},
+      create: {
+        name: clientName,
+        assistantId: assistant.id,
+        atbToken: atbToken,
+        atbBaseId: atbBaseId,
+        atbTableNM: atbTableNM,
+        atbTableId: atbTableId,
+      }
+    });
+    console.log({ testClient })
+  }
 }
 main()
   .then(async () => {
